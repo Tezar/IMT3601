a68a06f5d1c02cd080d69069e468fdeaac661965
Ales Tomecek <TomecekAles@seznam.cz>
2012-09-27 05:03:39 +0200

    Added Singleton template

---
+++ prototype-irrlicht/Irrlicht prototype/Irrlicht prototype/Singleton.hpp
@@ -0,0 +1,26 @@
+//source http://www.yolinux.com/TUTORIALS/C++Singleton.html
+
+#ifndef __SINGLETON_HPP_
+#define __SINGLETON_HPP_
+
+template <class T>
+class Singleton
+{
+public:
+  static T* getInstance() {
+      if(!m_pInstance) m_pInstance = new T;
+      assert(m_pInstance !=NULL);
+      return m_pInstance;
+  }
+protected:
+  Singleton();
+  ~Singleton();
+private:
+  Singleton(Singleton const&);
+  Singleton& operator=(Singleton const&);
+  static T* m_pInstance;
+};
+
+template <class T> T* Singleton<T>::m_pInstance=NULL;
+
+#endif
