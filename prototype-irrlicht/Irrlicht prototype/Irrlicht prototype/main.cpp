916d3b8742412eb099cd1a533d30d673ed1f9b7e
Tezar <TomecekAles@seznam.cz>
2012-10-13 11:58:07 +0200

    purified main litle bit more, moved CongruentGenerator to Track filter

--- prototype-irrlicht/Irrlicht prototype/Irrlicht prototype/main.cpp
+++ prototype-irrlicht/Irrlicht prototype/Irrlicht prototype/main.cpp
@@ -1,17 +1,6 @@
 #include "GameManagerClass.hpp"
 
 
-using namespace irr;
-
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
-
-#ifdef _IRR_WINDOWS_
-#pragma comment(lib, "Irrlicht.lib")
-#endif
 
 int main()
 {
