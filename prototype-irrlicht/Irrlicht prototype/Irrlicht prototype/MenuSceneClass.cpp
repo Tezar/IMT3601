50f558ed26788ee4ab4cd864452450d352539724
BoMann <reptile130@hotmail.com>
2012-10-17 07:00:03 -0700

    Updated work on the MenuSceneClass

--- prototype-irrlicht/Irrlicht prototype/Irrlicht prototype/MenuSceneClass.cpp
+++ prototype-irrlicht/Irrlicht prototype/Irrlicht prototype/MenuSceneClass.cpp
@@ -2,7 +2,8 @@
 parts based on irrlich example01
 */
 #include "MenuSceneClass.hpp"
-
+#include "GameManagerClass.hpp"
+#include "StartMenuUi.h"
 
 //todo: remove unused
 using namespace core;
@@ -10,6 +11,12 @@ using namespace scene;
 using namespace video;
 using namespace io;
 using namespace gui;
+
+using namespace irr;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#endif
 
 MenuSceneClass::MenuSceneClass(void)
 {
@@ -22,63 +29,40 @@ MenuSceneClass::~MenuSceneClass(void)
 
 void MenuSceneClass::onEnter()
 {
+	//video::E_DRIVER_TYPE driverType=irr::video::E_DRIVER_TYPE(1); //never put too "0"
+        /*if (driverType==video::EDT_COUNT)
+                return 1;*/
 
-	IrrlichtDevice * device = GameManager::getInstance()->getDevice();
+        // create device and exit if creation failed
 
-	/*
-	Get a pointer to the VideoDriver, the SceneManager and the graphical
-	user interface environment, so that we do not always have to write
-	device->getVideoDriver(), device->getSceneManager(), or
-	device->getGUIEnvironment().
-	*/
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	
-	/*
-	To show something interesting, we load a Quake 2 model and display it.
-	We only have to get the Mesh from the Scene Manager with getMesh() and add
-	a SceneNode to display the mesh with addAnimatedMeshSceneNode(). We
-	check the return value of getMesh() to become aware of loading problems
-	and other errors.
+        IrrlichtDevice * device = GameManager::getInstance()->getDevice();
 
-	Instead of writing the filename sydney.md2, it would also be possible
-	to load a Maya object file (.obj), a complete Quake3 map (.bsp) or any
-	other supported file format. By the way, that cool Quake 2 model
-	called sydney was modelled by Brian Collins.
-	*/
-	IAnimatedMesh* mesh = smgr->getMesh("../../media/sydney.md2");
-	if (!mesh)
-	{
-		device->drop();
-	}
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
+        video::IVideoDriver* driver = device->getVideoDriver();
+        IGUIEnvironment* env = device->getGUIEnvironment();
+		IGUISkin* skin = env->getSkin();
 
-	/*
-	To let the mesh look a little bit nicer, we change its material. We
-	disable lighting because we do not have a dynamic light in here, and
-	the mesh would be totally black otherwise. Then we set the frame loop,
-	such that the predefined STAND animation is used. And last, we apply a
-	texture to the mesh. Without it the mesh would be drawn using only a
-	color.
-	*/
-	if (node)
-	{
-		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture( 0, driver->getTexture("../../media/sydney.bmp") );
-	}
+        skin->setFont(env->getBuiltInFont(), EGDF_TOOLTIP);
+		env->addButton(rect<s32>(200,100,460,100 + 72), 0, GUI_ID_START_BUTTON,
+                        L"Start Program", L"Starts the program");
+        env->addButton(rect<s32>(200,180,460,180 + 72), 0, GUI_ID_QUIT_BUTTON,
+                        L"Quit", L"terminates the current program");
+        env->addButton(rect<s32>(200,260,460,260 + 72), 0, GUI_ID_FILE_OPEN_BUTTON,
+                        L"File Open", L"Opens a file");
 
-	/*
-	To look at the mesh, we place a camera into 3d space at the position
-	(0, 30, -40). The camera looks from there to (0,5,0), which is
-	approximately the place where our md2 model is.
-	*/
-	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
 
-	/*
-	Ok, now we have set up the scene
-	*/
+		//while(device->run() && driver)
+  //      if (device->isWindowActive())
+  //      {
+  //              driver->beginScene(true, true, SColor(0,200,200,200));
 
+  //              env->drawAll();
+  //      
+  //              driver->endScene();
+  //      }
+
+  //      device->drop();
+
+        /*return 0;*/
 }
 
 int MenuSceneClass::onFrame(int toDo){
