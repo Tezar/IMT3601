a5fa0326c58282d838caf922294e10312709b1c3
Tezar <TomecekAles@seznam.cz>
2012-11-11 14:40:00 +0100

    upgraded to irrlich version 1.8. Fixed few warning

--- prototype-irrlicht/Irrlicht prototype/Irrlicht prototype/CongruentGenerator.cpp
+++ prototype-irrlicht/Irrlicht prototype/Irrlicht prototype/CongruentGenerator.cpp
@@ -20,7 +20,7 @@ void CongruentGenerator::seed(irr::u32 newValue)
 irr::u32 CongruentGenerator::random(int maxNumber)
 {
 	currentSeed = currentSeed * 1103515245 + 12345;
-	irr:f32 f = (currentSeed/65536) % 32768;
-	f = f/32768.0*maxNumber;
+	irr::f32 f = irr::f32((currentSeed/65536) % 32768);
+	f = irr::f32(f/32768.0*maxNumber);
     return irr::u32(f) ;
 }
