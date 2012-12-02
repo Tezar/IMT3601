8008650fc147d593c6de43fd582d2133f5492574
Tezar <TomecekAles@seznam.cz>
2012-12-02 18:34:36 +0100

    added special body state for vehicle

--- prototype-irrlicht/game engine/EngineBodyState.hpp
+++ prototype-irrlicht/game engine/EngineBodyState.hpp
@@ -43,3 +43,33 @@ protected:
     btTransform mPos1;
 };
 
+
+
+  
+class EngineVehicleState: public EngineBodyState {
+public:
+	EngineVehicleState(Engine * e, Vehicle* _vehicle, const btTransform &initialpos): EngineBodyState(e,0, initialpos) {
+		vehicle = _vehicle;
+	}
+    virtual void setWorldTransform(const btTransform &worldTrans) {
+		engine->notifyShapeUpdate(btBody->getCollisionShape(), worldTrans);
+		mPos1 = worldTrans;
+
+
+
+		for (int i=0;i<vehicle->pointer->getNumWheels();i++)
+		{
+			//synchronize the wheels with the (interpolated) chassis worldtransform
+			vehicle->pointer->updateWheelTransform(i,true);
+			//draw wheels (cylinders)
+			engine->notifyShapeUpdate(vehicle->shapes[i], vehicle->pointer->getWheelInfo(i).m_worldTransform );
+		}
+
+
+    }
+
+
+protected:
+	Vehicle* vehicle;
+};
+
